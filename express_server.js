9f971473a68bc091092e8744793e523dd28bea03
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-09-02 03:02:40 +0000

    Saved the POST request for longURL and the generated shortURL into the urlDatabase. Redirected to the shortURL page. Created a new shortURL route to link to the longURL page

--- express_server.js
+++ express_server.js
@@ -37,9 +37,15 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); 
-  res.send("Ok");         
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);         
 });
 
 app.listen(PORT, () => {
