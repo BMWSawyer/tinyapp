0535bb07225e6743363c4f7db35d01dd17efb832
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-09-04 18:23:36 +0000

    Updated logic so that only logged in users can create new tiny URLs

--- express_server.js
+++ express_server.js
@@ -85,11 +85,15 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const user = users[req.cookies["user_id"]];
   
-  const templateVars = { 
-    user: user
-  };
-
-  res.render("urls_new", templateVars);
+  if (!user) {
+    res.redirect("/login");
+  } else {
+    const templateVars = { 
+      user: user
+    };
+  
+    res.render("urls_new", templateVars);
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
