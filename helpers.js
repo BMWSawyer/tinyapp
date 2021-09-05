a1a031a2daf85daba717a2871532464bdf8172f3
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-09-05 05:29:05 +0000

    Added some comments in the file

--- helpers.js
+++ helpers.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 
 const generateRandomString = function() {
+  // I found the logic for the random string generator at https://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript/22028809
+  // After I found it I made sure to research and read each method I didn't know about so that I understood everything that was happening before adding it in.
   let shortURL = Math.round((Math.pow(36, 6 + 1) - Math.random() * Math.pow(36, 6))).toString(36).slice(1);
 
   return shortURL;
@@ -8,7 +10,7 @@ const generateRandomString = function() {
 
 const checkEmail = function(email, users) {
   for (const user in users) {
-    if (users[user].email !== email) {
+    if (users[user].email !== email || users === undefined) {
       return true;
     }
     return false;
@@ -28,18 +30,18 @@ const urlsForUser = function(id, urlDatabase) {
   const userURLsObj = {};
   
   for (const shortURL in urlDatabase) {
-
+  
     if (id === urlDatabase[shortURL].userID) {
       userURLsObj[shortURL] = {
         "shortURL": shortURL,
         "longURL": urlDatabase[shortURL].longURL,
         "userID": urlDatabase[shortURL].userID
       };
-
-      return userURLsObj;
+    } else {
+      return null;
     }
   }
-  return null; 
+  return userURLsObj; 
 }
 
 const verifyUser = function(id, urlDatabase) { 
@@ -52,7 +54,7 @@ const verifyUser = function(id, urlDatabase) {
   return false; 
 }
 
-//This function is not used in the main project and is only here for the mocha and chai testing assignment
+// This function is not used in the main project and is only here for the mocha and chai testing assignment
 const getUserByEmail = function(email, usersDatabase) {
   for (const user in usersDatabase) {
     if(usersDatabase[user].email === email){
